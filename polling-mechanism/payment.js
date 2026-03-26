ff1527daea685c43c27dc851cd55dcd590f5efa1
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2026-03-27 02:24:25 +0530

    use promis with polling func for making it controlleable

--- polling-mechanism/payment.js
+++ polling-mechanism/payment.js
@@ -1,21 +1,36 @@
-const polling = (paymentId)=>{
-    const interval = setInterval(async()=>{
-          const res = await fetch(`/payment-status/${paymentId}`);
-          const data = await res.json();
+const polling = (paymentId) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(async () => {
+      try {
+        const res = await fetch(`/payment-status/${paymentId}`);
+        const data = await res.json();
 
-          if(data.status === "SUCCESS" || data.status === "FAILED"){
+        if (data.status === "SUCCESS" || data.status === "FAILED") {
           clearInterval(interval);
-          return data;
-          }
-    },3000);
-}
-const startPayment = async (amount)=>{
-    const response = await  fetch("/payment", {amount});
-    const { orderId } = await response.json();
-    return orderId;
-}
+          console.log(data);
+          resolve(data);
+        }
+      } catch (err) {
+        clearInterval(interval);
+        console.log(err);
+        reject(err);
+      }
+    }, 3000);
+  });
+};
+const startPayment = async (amount) => {
+  const response = await fetch("/payment", { amount });
+  const { orderId } = await response.json();
+  return orderId;
+};
 
-const orderId = startPayment(100);
+(async ()=>{
+    try{
+        const orderId = await startPayment(100);
+        const result = await polling(orderId);
 
-polling(orderId);
-
+        console.log('Payment success : ', result)
+    }catch(err){
+        console.log("Payment failed : ", err )
+    }
+})()
